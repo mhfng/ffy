2f7c0e12c2f7546598d1a770b61330178788c0c7
Gomaa <137703660+mhfng@users.noreply.github.com>
2023-06-30 00:04:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,21 @@
-
-from flask import Flask, render_template, request
-import base64
-import cv2
-import numpy as np
+from flask import Flask
 import telegram
+import sched
+import time
+import os
 
 app = Flask(__name__)
+bot = telegram.Bot(token=os.environ.get('5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'))
 
-bot = telegram.Bot(token='5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg')
+def send_message():
+    chat_id = os.environ.get('373715044')
+    message = 'Hello, this is a scheduled message!'
+    bot.send_message(chat_id=chat_id, text=message)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def schedule_message():
+    s.enter(3 * 60 * 60, 1, schedule_message)
+    send_message()
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    dataURL = request.get_data()
-    # convert data URL to OpenCV image
-    img_str = dataURL.split(',')[1]
-    img_data = bytes(img_str, 'utf-8')
-    nparr = np.frombuffer(base64.b64decode(img_data), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    # process the image here
-    # for example, save the image to disk
-    cv2.imwrite('frame.jpg', img)
-    # send the image to the Telegram bot
-    with open('frame.jpg', 'rb') as f:
-        bot.send_photo(chat_id='localipy', photo=f)
-    return 'OK'
-
-if __name__ == '__main__':
-    app.run()
-
+s = sched.scheduler(time.time, time.sleep)
+schedule_message()
+s.run()
